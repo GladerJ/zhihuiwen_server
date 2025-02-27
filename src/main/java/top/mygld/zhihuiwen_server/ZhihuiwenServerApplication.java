a73051775f8ad33c72698436145b4e219cd44274
chuan <1417344239@qq.com>
2025-02-27 18:03:16 +0800

    init

---
+++ src/main/java/top/mygld/zhihuiwen_server/ZhihuiwenServerApplication.java
@@ -0,0 +1,13 @@
+package top.mygld.zhihuiwen_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuiwenServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuiwenServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/mygld/zhihuiwen_server/ZhihuiwenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mygld.zhihuiwen_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhihuiwenServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
